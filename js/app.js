53c7268af09a22ace25e503128f2c2e99e9e167c
khaled-web <eng.khaled_ok@outlook.com>
2021-07-07 15:10:39 +0200

    Essa Agency Project is physically completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 /* Set Year */
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+ link.addEventListener("click", e => {
+  // prevent default
+  e.preventDefault();
+
+  const id = e.target.getAttribute("href").slice(1);
+  const element = document.getElementById(id);
+  //
+  let position = element.offsetTop - 62;
+
+  window.scrollTo({
+   left: 0,
+   top: element.offsetTop,
+   top: position,
+   behavior: "smooth"
+  });
+ });
+});
